2aeb8780c734f4db0146ed5e4953f84a698743fd
asammarco <asammarco@hotmail.com>
2021-03-11 08:34:04 -0300

    Initial Commit

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,10 @@
+export function RepositoryItem(props){
+  const {name, description, html_url} = props.repository;
+  return (
+    <li>
+        <strong>{name}</strong>
+        <p>{description}</p>
+        <a href={html_url}>Acessar repositório</a>
+      </li>
+  );
+}
